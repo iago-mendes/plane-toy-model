19ef5f53efd56fe835f259df64719944b30efbd6
Iago Mendes <ibrazmen@oberlin.edu>
2025-03-12 13:24:24 -0400

    Experiment with constraint damping

--- create_animation.sh
+++ create_animation.sh
@@ -2,13 +2,12 @@
 
 if [ -z "$1" ]
 then
-  echo "No given pattern"
-  exit
-elif [ -z "$2" ]
-then
   echo "No given filename"
   exit
 fi
 
 cd iterations
-ffmpeg -f image2 -r 10 -pattern_type glob -i "$1-*.png" -vcodec libx264 -crf 22 -pix_fmt yuv420p ../animations/$1-$2.mp4
+ffmpeg -f image2 -r 2 -pattern_type glob -i "physical_commutator-*.png" -vcodec libx264 -crf 22 -pix_fmt yuv420p \
+       ../animations/physical_commutator-$1.mp4
+ffmpeg -f image2 -r 2 -pattern_type glob -i "physical_dyad_vectors-*.png" -vcodec libx264 -crf 22 -pix_fmt yuv420p \
+       ../animations/physical_dyad_vectors-$1.mp4
--- relaxation.py
+++ relaxation.py
@@ -9,13 +9,12 @@ plt.rcParams.update({
 })
 
 # Define grid
-N = 100
+N = 10
 r_values = np.linspace(0.1, 10., N)
 phi_values = np.linspace(0., 2. * np.pi, 2 * N, endpoint=False)
 dr = r_values[1] - r_values[0]
 dphi = phi_values[1] - phi_values[0]
 r, phi = np.meshgrid(r_values, phi_values, indexing='ij')
-sigma = np.log(r)
 
 # Ploting grid
 x = r * np.cos(phi)
@@ -29,11 +28,14 @@ conformal_e_sigma = np.array([ np.cos(phi), np.sin(phi) ])
 conformal_e_phi = np.array([ -np.sin(phi), np.cos(phi) ])
 
 # Evolution parameters
-dt = 1e-2
-max_iterations = 1e3
+dt = 0.01 * (10/N)**2
+# max_iterations = 1
+max_iterations = int(200. / dt)
+relaxation_parameter = 0.
+damping_parameter = 1.
 
 # Create plots
-def plot_physical_dyad_vectors(iteration=0, show=False, commutator_norm=None):
+def plot_physical_dyad_vectors(iteration=0, show=False, commutator=None, constraint=None):
   physical_e_r = conformal_e_sigma
   physical_e_phi = Omega * conformal_e_phi
 
@@ -51,8 +53,8 @@ def plot_physical_dyad_vectors(iteration=0, show=False, commutator_norm=None):
     ax.set_aspect('equal')
     ax.grid('on', linestyle='--', alpha=0.5)
 
-  if commutator_norm is not None:
-    plt.suptitle(f'relaxation time = {iteration*dt:.1f}, physical commutator norm = {commutator_norm:.2e}')
+  if commutator is not None and constraint is not None:
+    plt.suptitle(f'relaxation time = {iteration*dt:.1f}, commutator = {commutator:.2e}, constraint = {constraint:.2e}')
 
   fig.set_size_inches(15, 8)
 
@@ -78,7 +80,7 @@ def plot_physical_commutator(commutator, iteration=0, show=False, commutator_nor
     ax.set_ylabel(r'$r$')
 
   if commutator_norm is not None:
-    plt.suptitle(f'relaxation time = {iteration*dt:.1f}, physical commutator norm = {commutator_norm:.2e}')
+    plt.suptitle(f'relaxation time = {iteration*dt:.1f}, physical commutator vector_norm = {commutator_norm:.2e}')
 
   fig.set_size_inches(15, 8)
 
@@ -91,9 +93,18 @@ def plot_physical_commutator(commutator, iteration=0, show=False, commutator_nor
 
 # Compute partial derivatives of 2D vector fields
 def partial_r(vector_field):
-  return np.gradient(vector_field, dr, axis=1)
+  # Centered FD
+  df = 1./2. * np.roll(vector_field, -1, axis=1) - 1./2. * np.roll(vector_field, 1, axis=1)
+  for xy in range(2):
+    for j in range(2*N):
+      # Forward FD
+      df[xy,0,j] = -1. * vector_field[xy,0,j] + 1. * vector_field[xy,1,j]
+      # Backward FD
+      df[xy,-1,j] = -1. * vector_field[xy,-2,j] + 1. * vector_field[xy,-1,j]
+  return df / dr
 def partial_phi(vector_field):
-  return (-1./2. * np.roll(vector_field, -1, axis=2) + 1./2. * np.roll(vector_field, 1, axis=2)) / dphi
+  df = 1./2. * np.roll(vector_field, -1, axis=2) - 1./2. * np.roll(vector_field, 1, axis=2)
+  return df / dphi
 
 # Compute commutators
 def conformal_commutator():
@@ -101,20 +112,105 @@ def conformal_commutator():
 def physical_commutator():
   return Omega**2 / 2. * partial_r(conformal_e_phi) - Omega / 2. * partial_phi(conformal_e_sigma) + Omega / 2. * conformal_e_phi
 
-# Compute dyad updates
-def dot_e_sigma():
-  return - partial_phi(conformal_commutator()) + partial_phi(conformal_e_phi)
-def dot_e_phi():
-  return Omega * partial_r(conformal_commutator()) - partial_phi(conformal_e_sigma) + conformal_e_phi
+# Compute integrals
+def integrate(field):
+  result = 0.
+  for i in range(N):
+    dA = r_values[i] * dr * dphi
+    for j in range(2*N):
+      result += field[i,j] * dA
+  return result
+def vector_norm(vector_field): # rank 1
+  sqr_norm = vector_field[0]**2 + r**2 * vector_field[1]**2
+  return np.sqrt(integrate(sqr_norm))
+def tensor_norm(field_ss, field_sp, field_pp): # rank 2
+  sqr_norm = r**4 * (field_ss**2 + 2. * field_sp**2 + field_pp**2)
+  # print(sqr_norm)
+  return np.sqrt(integrate(sqr_norm))
+
+# Metric components
+A = 0.6 # "stretchiness"
+def g_ss():
+  # return np.sqrt(1. / A**2 * np.cos(phi)**2 + A**2 * np.sin(phi)**2)
+  return r**2 * np.sqrt(1. / A**2 * np.cos(phi)**2 + A**2 * np.sin(phi)**2)
+def g_sp():
+  # return r * (A**2 - 1. / A**2) * np.sin(phi) * np.cos(phi)
+  return r**2 * (A**2 - 1. / A**2) * np.sin(phi) * np.cos(phi)
+def g_pp():
+  return r**2 / (1. / A**2 * np.cos(phi)**2 + A**2 * np.sin(phi)**2) * ( 1. + (A**2 - 1. / A**2)**2 * np.sin(phi)**2 * np.cos(phi)**2)
+
+# Constraints
+def C_ss():
+  return r**2 * (conformal_e_sigma[0] * conformal_e_sigma[0] + conformal_e_sigma[1] * conformal_e_sigma[1]) - g_ss()
+def C_sp():
+  return r**2 * (conformal_e_sigma[0] * conformal_e_phi[0] + conformal_e_sigma[1] * conformal_e_phi[1]) - g_sp()
+def C_pp():
+  return r**2 * (conformal_e_phi[0] * conformal_e_phi[0] + conformal_e_phi[1] * conformal_e_phi[1]) - g_pp()
+
+# Dyad updates
+def dot_e_sigma(C_ss, C_sp):
+  return relaxation_parameter * (- partial_phi(conformal_commutator()) - partial_phi(conformal_e_phi)) - damping_parameter / Omega**2 * (C_ss * conformal_e_sigma + C_sp * conformal_e_phi)
+  # return - damping_parameter / Omega**2 * (C_ss * conformal_e_sigma + C_sp * conformal_e_phi)
+def dot_e_phi(C_sp, C_pp):
+  return relaxation_parameter * (Omega * partial_r(conformal_commutator()) + partial_phi(conformal_e_sigma) - conformal_e_phi)  - damping_parameter / Omega**2 * (C_sp * conformal_e_sigma + C_pp * conformal_e_phi)
+  # return - damping_parameter / Omega**2 * (C_sp * conformal_e_sigma + C_pp * conformal_e_phi)
 
 # Evolution
+relaxation_time = []
+commutator_residuals = []
+constraint_residuals = []
 for i in range(int(max_iterations)):
-  if i % 10 == 0:
-    commutator = physical_commutator()
-    commutator_norm = np.linalg.norm(commutator)
-    print(f'{i}: physical commutator norm = {commutator_norm:.2e}')
-    plot_physical_dyad_vectors(iteration=i, commutator_norm=commutator_norm)
-    plot_physical_commutator(commutator, iteration=i, commutator_norm=commutator_norm)
+  constraint_ss = np.array([C_ss()] * 2)
+  constraint_sp = np.array([C_sp()] * 2)
+  constraint_pp = np.array([C_pp()] * 2)
 
-  conformal_e_sigma += dt * dot_e_sigma()
-  conformal_e_phi += dt * dot_e_phi()
+  if (i*dt) > 100.:
+    relaxation_parameter = 1e-5
+    damping_parameter = 1e-1
+  if (i*dt) > 110.:
+    relaxation_parameter = 1e-4
+    damping_parameter = 1e-2
+  if (i*dt) > 120.:
+    relaxation_parameter = 1e-3
+    damping_parameter = 1e-3
+  if (i*dt) > 130.:
+    relaxation_parameter = 1e-2
+    damping_parameter = 1e-4
+  if (i*dt) > 140.:
+    relaxation_parameter = 1e-1
+    damping_parameter = 1e-5
+  if (i*dt) > 150.:
+    relaxation_parameter = 1.
+    damping_parameter = 0.
+  
+  if i % 10 == 0:
+    relaxation_time.append(i * dt)
+
+    commutator = physical_commutator()
+    commutator_residual = vector_norm(commutator)
+    commutator_residuals.append(commutator_residual)
+
+    constraint_residual = tensor_norm(constraint_ss[0], constraint_sp[0], constraint_pp[0])
+    constraint_residuals.append(constraint_residual)
+
+    print(f'{i*dt:.3f}: commutator = {commutator_residual:.2e}, constraint = {constraint_residual:.2e}')
+
+    # plot_physical_dyad_vectors(iteration=i, commutator=commutator_residual, constraint=constraint_residual)
+    # plot_physical_commutator(commutator, iteration=i, commutator_norm=commutator_residual)
+  
+  conformal_e_sigma += dt * dot_e_sigma(constraint_ss, constraint_sp)
+  conformal_e_phi += dt * dot_e_phi(constraint_sp, constraint_pp)
+
+# Plot residuals
+fig, ax = plt.subplots(1, 1, squeeze=True)
+ax.set_title('Residuals')
+ax.plot(relaxation_time, commutator_residuals, label='Commutator')
+ax.plot(relaxation_time, constraint_residuals, label='Constraint')
+ax.set_xlabel('Relaxation time')
+ax.set_yscale('log')
+ax.legend()
+ax.grid('on', linestyle='--', alpha=0.5)
+fig.set_size_inches(16,8)
+plt.tight_layout()
+fig.savefig(f'residuals.png', format='png', bbox_inches='tight')
+plt.show()
